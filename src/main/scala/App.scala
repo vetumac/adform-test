ade65fcd6e9d40b12f8b84c7566eaf6d6187a67c
vetumac <alexei.suslov@instinctools.ru>
2016-02-18 09:31:09 +0300

    save

--- src/main/scala/App.scala
+++ src/main/scala/App.scala
@@ -1,6 +1,9 @@
+import scala.io.Source
+
 object App {
 
   def main(args: Array[String]) = {
-
+    val transactionsBuffer = Source.fromFile("transactions.tsv") //main/resources/transactions.tsv
+    transactionsBuffer.foreach(f => print(f))
   }
 }
